3173d0e4b881854b9c95c7cda6da97979f01628a
thomazvalentino <thomazvalentino321@gmail.com>
2026-06-02 16:20:18 -0300

    primeira versao dos sistema de faturamento

---
+++ código.py
@@ -0,0 +1,34 @@
+faturamento = 1000
+custo = 600
+lucro = faturamento - custo
+
+novas_vendas = 1000
+
+faturamento = faturamento + novas_vendas
+
+imposto = 0.15 * lucro
+
+
+
+
+
+print("imposto", imposto)
+print("faturamento", faturamento)
+print("custo", custo)
+print("lucro", faturamento - custo)
+
+mensagem = "O faturamento da loja é de 2000 reais " #string
+
+teve_lucro = True #boolean 
+
+margem_lucro = lucro / faturamento * 100
+print("margem" , margem_lucro)
+
+
+
+# int = numeros inteiros 
+# float = numeros casa decimal 
+# string = textos 
+# boolean = booleanos (true; False)
+
+ 
